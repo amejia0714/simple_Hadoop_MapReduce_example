4bccfdaa053abeabc568959c26eef702f015300c
amejia0714@gmail.com <amejia0714@gmail.com>
2021-02-07 22:07:19 +0000

    Updated to add stopwords from sklearn

---
+++ mapper3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+
+from sklearn.feature_extraction import stop_words
+stopwords = set(stop_words.ENGLISH_STOP_WORDS)
+
+# stopwords = set(['the', 'and']) 
+
+# get all lines from stdin
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip().lower()
+
+    # split the line into words; splits on any whitespace
+    words = line.split()
+
+    # output tuples (word, 1) in tab-delimited format
+    for word in words:
+	if word not in stopwords:
+	    print '%s\t%s' % (word, "1")
